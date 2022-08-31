d16541b9220ff9ebd0573adc614e608e90b71970
abhishek jain <jainabhishek347@gmail.com>
2022-08-31 16:52:01 +0530

    Login Failed fix

--- talmud.py
+++ talmud.py
@@ -1,19 +1,21 @@
 import sys
 import time
+import utils
+import config
 import warnings
 import pandas as pd
 from selenium import webdriver
-from selenium.webdriver import ActionChains
 from selenium.webdriver.common.by import By
+from selenium.webdriver import ActionChains
 from selenium. webdriver. common. keys import Keys
 from selenium.webdriver.chrome.options import Options
+from helpers.g_sheet_handler import GoogleSheetHandler
+from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.support.ui import WebDriverWait
+from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException, ElementClickInterceptedException, NoSuchElementException
 
-import config
-import utils
-from helpers.g_sheet_handler import GoogleSheetHandler
 
 warnings.filterwarnings("ignore")
 
@@ -32,10 +34,10 @@ class DataScrapping():
         print("Start user login..")
         try:
             self.browser.get(config.WEB_LINK)
-            self.browser.find_element_by_id('txtUserName').send_keys(self.username)
-          
-            self.browser.find_element_by_id('txtPassword').send_keys(self.password)
-            self.browser.find_element_by_id('LoginButton').click()
+            self.browser.find_element(By.ID,'txtUserName').send_keys(self.username)
+            # print("44444")
+            self.browser.find_element(By.ID,'txtPassword').send_keys(self.password)
+            self.browser.find_element(By.ID,'LoginButton').click()
             print("  Login Successful !!")
             self.user_login = True
         except:
@@ -49,7 +51,7 @@ class DataScrapping():
                 return False
             try:
                 print("\n\t = = = = = = = = = = [PAGE-NO: ", self.page_no, "] = = = = = = = = = =")
-                self.page = self.browser.find_element_by_id(f'ucTalmudSideBar_tvTalmudt{self.page_no}')
+                self.page = self.browser.find_element(By.ID, f'ucTalmudSideBar_tvTalmudt{self.page_no}')
                 self.page.click()
                 return True
             except:
@@ -63,12 +65,12 @@ class DataScrapping():
         for idx in range(len(passport_df)):
             branch = int(branch_code_df['branch'][idx][5:])+1
             self.browser.implicitly_wait(20)
-            self.browser.find_element_by_id(f'ucTalmudSideBar_tvTalmudn{branch}').click()
+            self.browser.find_element(By.ID,f'ucTalmudSideBar_tvTalmudn{branch}').click()
             self.browser.implicitly_wait(20)
             code_idx=4
             while True:
                 try:
-                    ele = self.browser.find_element_by_id(f'ucTalmudSideBar_tvTalmudt{code_idx}')
+                    ele = self.browser.find_element(By.ID,f'ucTalmudSideBar_tvTalmudt{code_idx}')
                     if branch_code_df['code'][idx] in ele.text:
                         ele.click()
                         break
@@ -77,9 +79,9 @@ class DataScrapping():
                     code_idx += 1
         
             self.browser.implicitly_wait(20)
-            self.browser.find_element_by_id('ContentPlaceHolder1_tabStudyTypeDetails_StudentList_tab').click()
+            self.browser.find_element(By.ID, 'ContentPlaceHolder1_tabStudyTypeDetails_StudentList_tab').click()
             self.browser.implicitly_wait(20)
-            self.browser.find_element_by_id('ContentPlaceHolder1_btnStudentAcceptence3').click()
+            self.browser.find_element(By.ID, 'ContentPlaceHolder1_btnStudentAcceptence3').click()
             self.browser.implicitly_wait(20); time.sleep(5)
             if passport_df['id_type'][idx] == 'דרכון':
                 WebDriverWait(self.browser, 10).until(EC.element_to_be_clickable((By.ID, 'ContentPlaceHolder1_tabStudyTypeDetails_StudentList_ucStudentsSearchDetails_ddlPopIdentityType'))).click()
@@ -105,6 +107,13 @@ class DataScrapping():
                 input = WebDriverWait(self.browser, 20).until(EC.visibility_of_element_located((By.XPATH, '/html/body/form/div[3]/div[5]/div[1]/div/div[6]/div[2]/div/div/div[2]/div[3]/div[5]/div/div/div/table/tbody/tr/td/table/tbody/tr[3]/td/input')))
                 input.send_keys(key)
             WebDriverWait(self.browser,10).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="ContentPlaceHolder1_tabStudyTypeDetails_StudentList_ucStudentsSearchDetails_LinkButton1"]'))).click()
+            try:
+                invalid = self.browser.find_element(By.ID, 'ContentPlaceHolder1_tabStudyTypeDetails_StudentList_ucStudentsSearchDetails_cvalID').text
+                if invalid == 'מספר זהות לא חוקי':
+                    WebDriverWait(self.browser, 10).until(EC.element_to_be_clickable((By.ID, 'ContentPlaceHolder1_tabStudyTypeDetails_StudentList_ucStudentsSearchDetails_LinkButton2'))).click()
+            except NoSuchElementException:
+                pass
+
             pop_up_text = WebDriverWait(self.browser,10).until(EC.visibility_of_element_located((By.ID, 'ucMessagePopUp_lblMessage')))
             print(pop_up_text.text)
             self.pop_up_text[passport_df['id'][idx]] = pop_up_text.text            
@@ -119,8 +128,8 @@ class DataScrapping():
 
     def logout(self):
         self.user_login = False
-        self.browser.find_element_by_id('ucTalmudHeader_ucLogOut_lnkLogOut').click(); time.sleep(3)
-        self.browser.find_element_by_id('ucTalmudHeader_ucLogOut_btnOk').click(); time.sleep(3)
+        self.browser.find_element(By.ID,'ucTalmudHeader_ucLogOut_lnkLogOut').click(); time.sleep(3)
+        self.browser.find_element(By.ID,'ucTalmudHeader_ucLogOut_btnOk').click(); time.sleep(3)
         print('Logged out user(%s) successfully!\n' %self.username)
         time.sleep(3)
 
@@ -128,7 +137,7 @@ class DataScrapping():
     def push_data_to_drive(self):
         print(f"\t\t[Pushing data to drive for user - {self.username}]")
         data = utils.flattened_data(self)
-        GoogleSheetHandler(data=data, sheet_name='STDUENTS').appendsheet_records()
+        GoogleSheetHandler(data=data, sheet_name=config.SHEET_NAME).appendsheet_records()
 
 if __name__=='__main__':
     args = len(sys.argv)
@@ -138,9 +147,9 @@ if __name__=='__main__':
         print('sys.argv:', sys.argv)
         """ Custom options for browser """ 
         options.headless = True
-        browser = webdriver.Chrome(executable_path=config.DRIVER_PATH, options=options)
+        browser = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
     else:
-        browser = webdriver.Chrome(executable_path=config.DRIVER_PATH)
+        browser = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 
     print(" * *  * *  * *  * *  * *  * * START  * *  * *  * *  * *  * * ")
     action = ActionChains(browser)
